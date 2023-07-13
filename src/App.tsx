feaa85dfd57e4a14b83c11390be62e3fdee88afb
Glen <59471231+GHarding95@users.noreply.github.com>
2023-07-13 16:06:23 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,39 @@ const App: React.FC = () => {
   const driverStandings = useDriverStandings();
   const currentYear = getCurrentYear();
 
-  return (
-    <>
-    <div className='driver-card-wrapper'>
-      <h1 className='font-bold'>F1 {currentYear} Drivers</h1>      
-      {driverStandings.map((standing, index) => (
-        <Card
-          key={index}
-          driverStanding={standing}
-          isFirstDriver={index === 0}
-          position={index + 1}
-        />
-      ))}
-  </div>
-    </>
+  const renderCards = () => {
+    const rows: JSX.Element[] = [];
+    const numDrivers = driverStandings.length;
+    const numColumns = 4;
+    const numRows = Math.ceil(numDrivers / numColumns);
+
+    for (let i = 0; i < numRows; i++) {
+      const start = i === 0 ? 0 : (i - 1) * numColumns + 3;
+      const end = i === 0 ? 3 : i * numColumns + 3;
+      const rowDrivers = driverStandings.slice(start, end);
+
+      rows.push(
+        <div className="flex mb-4" key={i}>
+          {rowDrivers.map((standing, index) => (
+            <Card
+              key={index}
+              driverStanding={standing}
+              isFirstDriver={index === 0 && i === 0}
+              position={start + index + 1}
+            />
+          ))}
+        </div>
+      );
+    }
+
+    return rows;
+  };
+
+  return (  
+    <div className="driver-card-wrapper">
+      <h1 className="font-bold text-3xl mb-4">F1 {currentYear} Drivers</h1>
+      {renderCards()}
+    </div>
   );
 };
 
